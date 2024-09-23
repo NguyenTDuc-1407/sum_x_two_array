45553483ab255149d1c236e8b60a5e1c83414bec
Duc.NT <311020ntd@gmail>
2024-09-23 14:46:33 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿const int x = 3, y = 3;
+int sum = 0;
+int[,] arr = new int[x, y] {
+                { 3, 2, 1 },
+                { 4, 2, 3 },
+                { 5, 1, 6 }
+            };
+
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        if (i == j)
+        {
+            sum = sum + arr[i, j];
+        }
+    }
+}
+Console.Write("Tong cac phan tu tren duong cheo chinh la: {0}\n", sum);
